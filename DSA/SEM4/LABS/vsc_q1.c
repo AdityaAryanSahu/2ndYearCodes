da8626529d5a948bcc436e8ed695c51ab67cc29c
AdityaAryanSahu <160647516+AdityaAryanSahu@users.noreply.github.com>
2025-03-24 11:10:16 +0530

    Add files via upload

---
+++ DSA/SEM4/LABS/vsc_q1.c
@@ -0,0 +1,161 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+int height(struct Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+struct Node* newNode(int key)
+{
+    struct Node* node = (struct Node*)
+                        malloc(sizeof(struct Node));
+    node->key   = key;
+    node->left   = NULL;
+    node->right  = NULL;
+    node->height = 1;  // new node is initially added at leaf
+    return(node);
+}
+
+struct Node *rightRotate(struct Node *y)
+{
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+
+    return x;
+}
+
+struct Node *leftRotate(struct Node *x)
+{
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(height(x->left),   
+                    height(x->right)) + 1;
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    return y;
+}
+
+int getBalance(struct Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+struct Node* insert(struct Node* node, int key)
+{
+
+    if (node == NULL)
+        return(newNode(key));
+
+    if (key < node->key)
+        node->left  = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else 
+        return node;
+
+    node->height = 1 + max(height(node->left),
+                        height(node->right));
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left =  leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void InOrder(struct Node *root)
+{
+    if(root != NULL)
+    {
+        InOrder(root->left);
+           printf("%d ", root->key);
+        InOrder(root->right);
+    }
+}
+void succ_pred(struct Node* root,int key){
+	struct Node*temp=root;
+	struct Node*prev=NULL;
+    while(temp){
+        if(key==temp->key){break;}
+    	else if(key<temp->key) {temp=temp->left;}
+    	else {temp=temp->right;}
+    }
+    if(temp){
+      struct Node* lnode=temp->left;
+         struct Node* rnode=temp->right;
+           while(lnode->right!=NULL){
+            lnode=lnode->right;
+         }
+         printf("pred is %d\n",lnode->key);
+         while(rnode->left!=NULL){
+            rnode=rnode->left;
+         }
+         printf("succ is %d\n",rnode->key);
+    }
+    else printf("key not found\n");
+}
+
+int main()
+{
+  struct Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  int val;
+
+  while(n>0){
+  	scanf("%d",&val);
+  	root=insert(root,val);
+  	n--;
+  }
+  InOrder(root);
+   printf("\nenter a key\n");
+  int key;
+  scanf("%d",&key);
+  succ_pred(root,key);
+}
+
+  
